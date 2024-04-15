3cedddae941ecb77cff3532a38ee7744e0cf905a
sujinee01 <kmosujin@gmail.com>
2024-04-15 15:12:32 +0900

    helloworld.py

--- prac6/ex1.py
+++ prac6/ex1.py
@@ -1,10 +1,13 @@
-    # // Python
-    def find_dog(sound): 
-       if sound == "멍멍":
-           return("개가 짖네") 
-       else:
-           return("다른 동물이구나") 
-       sound = "야옹" 
-       find_result = find_dog(sound) 
+# Python
 
-       print(find_result)
+def find_dog(sound): 
+    if sound == "멍멍":
+        return("개가 짖네") 
+    else:
+        return("다른 동물이구나")
+    
+sound = "야옹"  
+find_result = find_dog(sound) 
+print(find_result)
+    
+# find_dog("멍멍")
